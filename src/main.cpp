55aefaa0f65697d13d437d393a2d449628b31cd4
Jan Rydval <jan@rydv.al>
2025-05-09 15:01:43 +0200

    Display double buffering added

--- src/main.cpp
+++ src/main.cpp
@@ -2,56 +2,68 @@
 #include <Adafruit_BMP280.h>
 #include "SHTSensor.h"
 
-// BMP280 senzoru obvykle používá I2C adresu 0x76 nebo 0x77
 Adafruit_BMP280 bmp;
 SHTSensor sht;
+LGFX_Sprite buffer(&M5Cardputer.Display);
+
+// Pole barev pro rotaci
+const uint16_t frameColors[] = { DARKGREY, LIGHTGREY };
+const int numColors = sizeof(frameColors) / sizeof(frameColors[0]);
+int colorIndex = 0;
 
 void setup() {
-  // Inicializace Cardputeru
   M5Cardputer.begin();
   M5Cardputer.Display.setTextSize(1);
-  M5Cardputer.Display.setFont(&FreeMonoBold12pt7b);
-  M5Cardputer.Display.setCursor(0, 0);
+  M5Cardputer.Display.setFont(&FreeMono12pt7b);
   M5Cardputer.Display.println("Init");
-  //M5Cardputer.Ex_I2C.begin();
-
-  // Inicializace I2C na portu A (GPIO1 = SDA, GPIO2 = SCL)
-  //Wire.begin(1, 2);
 
   if (!bmp.begin(0x76)) {
     M5Cardputer.Display.println("BMP Err");
     while (true) delay(10);
   }
-
-  if(!sht.init()) {
+  if (!sht.init()) {
     M5Cardputer.Display.println("SHT Err");
     while (true) delay(10);
   }
-
   M5Cardputer.Display.println("OK");
-  delay(1000);
-  M5Cardputer.Display.clear();
-  Serial.begin(115200);
+
+  delay(500);
+
+  buffer.createSprite(M5Cardputer.Display.width(), M5Cardputer.Display.height());
+  buffer.setTextSize(1);
+  buffer.setFont(&Font4);
 }
 
 void loop() {
-  auto disp = M5Cardputer.Display;
+  auto& disp = M5Cardputer.Display;
 
   float teplota = bmp.readTemperature();
-  float tlak = bmp.readPressure() / 100.0F; // Převod na hPa
-
+  float tlak = bmp.readPressure() / 100.0F;
   sht.readSample();
   float teplota2 = sht.getTemperature();
   float vlhkost2 = sht.getHumidity();
 
-  disp.setCursor(0, 0);
-  disp.clear();
-  disp.setTextColor(BLUE);
-  disp.printf("\n %.1f C\n", teplota);
-  disp.printf(" %.1f hPa\n", tlak);
-  disp.setTextColor(VIOLET);
-  disp.printf(" %.1f C\n", teplota2);
-  disp.printf(" %.1f %%\n", vlhkost2);
+  int w = disp.width();
+  int h = disp.height();
+
+  // Vyber barvu rámečku
+  uint16_t frameColor = frameColors[colorIndex];
+  colorIndex = (colorIndex + 1) % numColors;
+
+  buffer.fillScreen(BLACK);
+
+  buffer.drawRoundRect(2, 2, w - 4, h - 4, 8, frameColor);
+
+  buffer.setCursor(0, 17);
+  buffer.setTextColor(BLUE);
+  buffer.printf("    %.1f C\n", teplota);
+  buffer.printf("    %.1f hPa\n", tlak);
+
+  buffer.setTextColor(VIOLET);
+  buffer.printf("    %.1f C\n", teplota2);
+  buffer.printf("    %.1f %%\n", vlhkost2);
+
+  buffer.pushSprite(0, 0);
 
   Serial.printf("%.1f|%.1f|%.1f|%.1f\n", teplota, tlak, teplota2, vlhkost2);
 
